600f712a3c365bdbcfcf426bacdc616bfa4dac57
bandu3978 <120469400+bandu3978@users.noreply.github.com>
2023-03-10 08:24:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import StudentDetails from "./JSON in PROPS/StudentDetails.jsx";
+import json from "./JSON in PROPS/student.json"
+import "./JSON in PROPS/globle.css"
+
+
+
+const App=()=>
+{
+  return(
+  
+<>
+
+   <StudentDetails data1={json} IMG1="https://www.shutterstock.com/image-photo/portrait-smiling-young-college-student-260nw-1192615495.jpg"
+   IMG2="https://alis.alberta.ca/media/699167/university-student-studying-library-istock-871203840.jpg"
+   IMG3="https://www.fastweb.com/uploads/article_photo/photo/2036641/crop635w_10-ways-to-be-a-better-student.jpeg"
+   IMG4="https://d2jyir0m79gs60.cloudfront.net/news/images/successful-college-student-lg.png"
+   IMG5="https://img.freepik.com/free-photo/young-attractive-smiling-student-showing-thumb-up-outdoors-campus-university_8353-6394.jpg?w=2000"
+   IMG6="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg"
+   IMG7="https://i2.wp.com/rollercoasteryears.com/wp-content/uploads/Thrive-During-Finals-.jpg?fit=1000%2C667&ssl=1"
+
+   IMG8="https://assets.citynavigator.nl/thumb/p3tsSJLLYpsERDYz8kA1puN81iy5OB_1v71l2zLtD-c/resizing_type:fit/width:960/height:0/gravity:sm/enlarge:0/aHR0cHM6Ly9hc3NldHMuY2l0eW5hdmlnYXRvci5ubC9rdW1hLWl0Yy91cGxvYWRzL21lZGlhLzYxNmVjOWNjZGJjNjAvc2h1dHRlcnN0b2NrLWdlc3BpZWdlbGQuanBn.jpg"
+   IMG9="https://www.thebalancemoney.com/thmb/Ci8l5i-cl3_emjyarN7RVunlULo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/enjoying-his-studies-484286872-735782e8d2144f7496bf9b947bcc1a56.jpg"
+   IMG10="https://media.istockphoto.com/id/1310896133/photo/happy-smiling-afro-businessman-using-laptop-at-the-desk-in-office.jpg?s=612x612&w=0&k=20&c=QUQpwqaaPswBIJB6_lk5xU6HY_RevWNSwcqAErPZsOc="
+   IMG11="https://www.thoughtco.com/thmb/V-FbvAWybmcx5xDJir0i4x9Qhto=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-157522427-5c8d8df046e0fb000187a2d9.jpg"
+   IMG12="https://forrit-one-msedu-p1-consumables.azureedge.net/media/30cd7de8-a992-4208-a289-18b445e93405/StudentsOverview-1920_1040x585.jpg"
+   IMG13="https://www.psychologicalscience.org/redesign/wp-content/uploads/2016/10/students-e1477942832896-609x419.jpg"
+   IMG14="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4q2p7lLCb97FBU3JB1mma1w_nZJgAGuwdxw&usqp=CAU"
+   IMG15="https://www.ellucian.com/sites/default/files/styles/max_width_2400/public/uploads/images/2022/04/social-image-student-information-systems.jpg"
+   IMG16="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/08/international.jpeg-1.jpg"
+   IMG17="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfj6DaaDeJS2t3axtESFKmDxq_IgbVFCwpzg&usqp=CAU"
+   IMG18="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDAa-FmwpNVZsEZZ8z9YqzY32QOQB09yCisA&usqp=CAU"
+   IMG19="https://www.wur.nl/upload_mm/d/d/8/0a659dd3-6a79-4f92-a923-c417a57a8233_student_studeren_bieb_bibliotheek_lezen_640x430_350e4bdf_640x430_04510857_640x430.jpg"
+   IMG20="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQru1atBJ7X5wTkKiD-kQXNVHiWCPOo1anNqQ&usqp=CAU"
+
+
+/> 
+   </>
+ 
+  )
+}
+export default App
---
+++ StudentDetails.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+
+const StudentDetails=(props)=>
+{
+  return(
+    <>
+ <div className="main">
+      <div className="row">
+        <div className="box">
+    <img src={props.IMG1} alt="text" />
+    <h2>{props.data1[0].StudentName}</h2>
+    <h2>{props.data1[0].StudentEmail}</h2>
+    <h2>{props.data1[0].Mobilenumber}</h2>
+    <h2>{props.data1[0].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG2} alt="text" />
+    <h2>{props.data1[1].StudentName}</h2>
+    <h2>{props.data1[1].StudentEmail}</h2>
+    <h2>{props.data1[1].Mobilenumber}</h2>
+    <h2>{props.data1[1].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG3} alt="text" />
+    <h2>{props.data1[2].StudentName}</h2>
+    <h2>{props.data1[2].StudentEmail}</h2>
+    <h2>{props.data1[2].Mobilenumber}</h2>
+    <h2>{props.data1[2].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG4} alt="text" />
+    <h2>{props.data1[3].StudentName}</h2>
+    <h2>{props.data1[3].StudentEmail}</h2>
+    <h2>{props.data1[3].Mobilenumber}</h2>
+    <h2>{props.data1[3].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG5} alt="text" />
+    <h2>{props.data1[4].StudentName}</h2>
+    <h2>{props.data1[4].StudentEmail}</h2>
+    <h2>{props.data1[4].Mobilenumber}</h2>
+    <h2>{props.data1[4].place}</h2>
+    </div>
+    
+     </div>
+
+     <div className="row">
+        <div className="box">
+    <img src={props.IMG6} alt="text" />
+    <h2>{props.data1[5].StudentName}</h2>
+    <h2>{props.data1[5].StudentEmail}</h2>
+    <h2>{props.data1[5].Mobilenumber}</h2>
+    <h2>{props.data1[5].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG7} alt="text" />
+    <h2>{props.data1[6].StudentName}</h2>
+    <h2>{props.data1[6].StudentEmail}</h2>
+    <h2>{props.data1[6].Mobilenumber}</h2>
+    <h2>{props.data1[6].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG8} alt="text" />
+    <h2>{props.data1[7].StudentName}</h2>
+    <h2>{props.data1[7].StudentEmail}</h2>
+    <h2>{props.data1[7].Mobilenumber}</h2>
+    <h2>{props.data1[7].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG9} alt="text" />
+    <h2>{props.data1[8].StudentName}</h2>
+    <h2>{props.data1[8].StudentEmail}</h2>
+    <h2>{props.data1[8].Mobilenumber}</h2>
+    <h2>{props.data1[8].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG10} alt="text" />
+    <h2>{props.data1[9].StudentName}</h2>
+    <h2>{props.data1[9].StudentEmail}</h2>
+    <h2>{props.data1[9].Mobilenumber}</h2>
+    <h2>{props.data1[9].place}</h2>
+    </div>
+    
+     </div>
+
+     <div className="row">
+        <div className="box">
+    <img src={props.IMG11} alt="text" />
+    <h2>{props.data1[10].StudentName}</h2>
+    <h2>{props.data1[10].StudentEmail}</h2>
+    <h2>{props.data1[10].Mobilenumber}</h2>
+    <h2>{props.data1[10].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG12} alt="text" />
+    <h2>{props.data1[11].StudentName}</h2>
+    <h2>{props.data1[11].StudentEmail}</h2>
+    <h2>{props.data1[11].Mobilenumber}</h2>
+    <h2>{props.data1[11].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG13} alt="text" />
+    <h2>{props.data1[12].StudentName}</h2>
+    <h2>{props.data1[12].StudentEmail}</h2>
+    <h2>{props.data1[12].Mobilenumber}</h2>
+    <h2>{props.data1[12].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG14} alt="text" />
+    <h2>{props.data1[13].StudentName}</h2>
+    <h2>{props.data1[13].StudentEmail}</h2>
+    <h2>{props.data1[13].Mobilenumber}</h2>
+    <h2>{props.data1[13].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG15} alt="text" />
+    <h2>{props.data1[14].StudentName}</h2>
+    <h2>{props.data1[14].StudentEmail}</h2>
+    <h2>{props.data1[14].Mobilenumber}</h2>
+    <h2>{props.data1[14].place}</h2>
+    </div>
+    
+     </div>
+
+
+     <div className="row">
+        <div className="box">
+    <img src={props.IMG16} alt="text" />
+    <h2>{props.data1[15].StudentName}</h2>
+    <h2>{props.data1[15].StudentEmail}</h2>
+    <h2>{props.data1[15].Mobilenumber}</h2>
+    <h2>{props.data1[15].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG17} alt="text" />
+    <h2>{props.data1[16].StudentName}</h2>
+    <h2>{props.data1[16].StudentEmail}</h2>
+    <h2>{props.data1[16].Mobilenumber}</h2>
+    <h2>{props.data1[16].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG18} alt="text" />
+    <h2>{props.data1[17].StudentName}</h2>
+    <h2>{props.data1[17].StudentEmail}</h2>
+    <h2>{props.data1[17].Mobilenumber}</h2>
+    <h2>{props.data1[17].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG19} alt="text" />
+    <h2>{props.data1[18].StudentName}</h2>
+    <h2>{props.data1[18].StudentEmail}</h2>
+    <h2>{props.data1[18].Mobilenumber}</h2>
+    <h2>{props.data1[18].place}</h2>
+    </div>
+    <div className="box">
+    <img src={props.IMG20} alt="text" />
+    <h2>{props.data1[19].StudentName}</h2>
+    <h2>{props.data1[19].StudentEmail}</h2>
+    <h2>{props.data1[19].Mobilenumber}</h2>
+    <h2>{props.data1[19].place}</h2>
+    </div>
+    
+     </div>
+
+
+
+
+
+ </div>
+    </>
+   
+    
+  )
+}
+export default StudentDetails
